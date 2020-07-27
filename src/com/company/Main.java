e7ffba6bcba20bc693e4f196c3bbee7f840e9000
leomenosse <leo_menosse@hotmail.com>
2020-07-27 11:05:33 -0300

    Implementação para tarefa do conteúdo 5.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,23 +8,71 @@ public class Main {
 
 //        Menu.show();
 
-        PGM og = new PGM("blurry_moon.pgm");
-        PGM pgm = new PGM(og);
+        PGM blurryMoonOriginal = new PGM("blurry_moon.pgm");
+        PGM blurringOriginal = new PGM("blurring.pgm");
+        PGM hubbleOriginal = new PGM("hubble.pgm");
 
-        int[][] filtro = new int[3][3];
-        filtro[0][0] = 0;
-        filtro[0][1] = -1;
-        filtro[0][2] = 0;
-        filtro[1][0] = -1;
-        filtro[1][1] = 4;
-        filtro[1][2] = -1;
-        filtro[2][0] = 0;
-        filtro[2][1] = -1;
-        filtro[2][2] = 0;
+        int[][] filtro1 = new int[3][3];
+        filtro1[0][0] = 0;
+        filtro1[0][1] = -1;
+        filtro1[0][2] = 0;
+        filtro1[1][0] = -1;
+        filtro1[1][1] = 4;
+        filtro1[1][2] = -1;
+        filtro1[2][0] = 0;
+        filtro1[2][1] = -1;
+        filtro1[2][2] = 0;
 
-        int[] array;
-        array = Util.matrixToArray(filtro);
-        pgm.filtroLaplaciano(filtro, 1);
-        pgm.save("blurry_moon_laplace.pgm");
+        int[][] filtro2 = new int[3][3];
+        filtro2[0][0] = -1;
+        filtro2[0][1] = -1;
+        filtro2[0][2] = -1;
+        filtro2[1][0] = -1;
+        filtro2[1][1] = 8;
+        filtro2[1][2] = -1;
+        filtro2[2][0] = -1;
+        filtro2[2][1] = -1;
+        filtro2[2][2] = -1;
+
+        /* FILTRO LAPLACIANO COM blurry_moon.pgm */
+        PGM blurryMoonFiltro = new PGM(blurryMoonOriginal);
+
+        int[][] imagemFiltrada = blurryMoonFiltro.filtroLaplaciano(filtro1);
+        blurryMoonFiltro.setPixels(imagemFiltrada);
+        blurryMoonFiltro.save("blurry_moon_filtro1.pgm");
+
+        blurryMoonFiltro.setPixels(blurryMoonOriginal.getPixels());
+        blurryMoonFiltro.alterarPixelsAposFiltragemLaplaciana(imagemFiltrada, 1);
+        blurryMoonFiltro.save("blurry_moon_resultado1.pgm");
+
+        blurryMoonFiltro = new PGM(blurryMoonOriginal);
+
+        imagemFiltrada = blurryMoonFiltro.filtroLaplaciano(filtro2);
+        blurryMoonFiltro.setPixels(imagemFiltrada);
+        blurryMoonFiltro.save("blurry_moon_filtro2.pgm");
+
+        blurryMoonFiltro.setPixels(blurryMoonOriginal.getPixels());
+        blurryMoonFiltro.alterarPixelsAposFiltragemLaplaciana(imagemFiltrada, 1);
+        blurryMoonFiltro.save("blurry_moon_resultado2.pgm");
+
+        /* Filtro da média com blurring.pgm E hubble.pgm */
+
+        int[] sizes = {3, 5, 9, 15, 35};
+
+        for(int s: sizes){
+            PGM blurringFiltro = new PGM(blurringOriginal);
+            imagemFiltrada = blurringFiltro.filtroMedia(s, s);
+            blurringFiltro.alterarPixelsAposFiltragemMedia(imagemFiltrada);
+            blurringFiltro.save(String.format("blurring_filtro%dx%d.pgm", s, s));
+
+            PGM hubbleFiltro = new PGM(hubbleOriginal);
+            imagemFiltrada = hubbleFiltro.filtroMedia(s, s);
+            hubbleFiltro.alterarPixelsAposFiltragemMedia(imagemFiltrada);
+            hubbleFiltro.save(String.format("hubble_filtro%dx%d.pgm", s, s));
+            if(s == 15){ //binarização
+                hubbleFiltro.fatiamento(0, 127, 0, 255);
+                hubbleFiltro.save("hubble_filtro15x15_binarizado.pgm");
+            }
+        }
     }
 }
