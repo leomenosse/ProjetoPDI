5ff3af3b9a396a5d1dfb69f1c298b1f30d07d4b1
leomenosse <leo_menosse@hotmail.com>
2020-08-13 22:18:19 -0300

    RGB, CYM e HSI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,72 +7,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 //        Menu.show();
+        String ppmName = "strawberries.ppm";
+        String nomeSemExtensao = ppmName.split("[.]")[0];
 
-        PGM blurryMoonOriginal = new PGM("blurry_moon.pgm");
-        PGM blurringOriginal = new PGM("blurring.pgm");
-        PGM hubbleOriginal = new PGM("hubble.pgm");
-
-        int[][] filtro1 = new int[3][3];
-        filtro1[0][0] = 0;
-        filtro1[0][1] = -1;
-        filtro1[0][2] = 0;
-        filtro1[1][0] = -1;
-        filtro1[1][1] = 4;
-        filtro1[1][2] = -1;
-        filtro1[2][0] = 0;
-        filtro1[2][1] = -1;
-        filtro1[2][2] = 0;
-
-        int[][] filtro2 = new int[3][3];
-        filtro2[0][0] = -1;
-        filtro2[0][1] = -1;
-        filtro2[0][2] = -1;
-        filtro2[1][0] = -1;
-        filtro2[1][1] = 8;
-        filtro2[1][2] = -1;
-        filtro2[2][0] = -1;
-        filtro2[2][1] = -1;
-        filtro2[2][2] = -1;
-
-        /* FILTRO LAPLACIANO COM blurry_moon.pgm */
-        PGM blurryMoonFiltro = new PGM(blurryMoonOriginal);
-
-        int[][] imagemFiltrada = blurryMoonFiltro.filtroLaplaciano(filtro1);
-        blurryMoonFiltro.setPixels(imagemFiltrada);
-        blurryMoonFiltro.save("blurry_moon_filtro1.pgm");
-
-        blurryMoonFiltro.setPixels(blurryMoonOriginal.getPixels());
-        blurryMoonFiltro.alterarPixelsAposFiltragemLaplaciana(imagemFiltrada, 1);
-        blurryMoonFiltro.save("blurry_moon_resultado1.pgm");
-
-        blurryMoonFiltro = new PGM(blurryMoonOriginal);
-
-        imagemFiltrada = blurryMoonFiltro.filtroLaplaciano(filtro2);
-        blurryMoonFiltro.setPixels(imagemFiltrada);
-        blurryMoonFiltro.save("blurry_moon_filtro2.pgm");
-
-        blurryMoonFiltro.setPixels(blurryMoonOriginal.getPixels());
-        blurryMoonFiltro.alterarPixelsAposFiltragemLaplaciana(imagemFiltrada, 1);
-        blurryMoonFiltro.save("blurry_moon_resultado2.pgm");
-
-        /* Filtro da média com blurring.pgm E hubble.pgm */
-
-        int[] sizes = {3, 5, 9, 15, 35};
-
-        for(int s: sizes){
-            PGM blurringFiltro = new PGM(blurringOriginal);
-            imagemFiltrada = blurringFiltro.filtroMedia(s, s);
-            blurringFiltro.alterarPixelsAposFiltragemMedia(imagemFiltrada);
-            blurringFiltro.save(String.format("blurring_filtro%dx%d.pgm", s, s));
-
-            PGM hubbleFiltro = new PGM(hubbleOriginal);
-            imagemFiltrada = hubbleFiltro.filtroMedia(s, s);
-            hubbleFiltro.alterarPixelsAposFiltragemMedia(imagemFiltrada);
-            hubbleFiltro.save(String.format("hubble_filtro%dx%d.pgm", s, s));
-            if(s == 15){ //binarização
-                hubbleFiltro.fatiamento(0, 127, 0, 255);
-                hubbleFiltro.save("hubble_filtro15x15_binarizado.pgm");
-            }
-        }
+        PPM ppm = new PPM(ppmName);
+        ppm.saveRGB(nomeSemExtensao);
+        ppm.saveCMY(nomeSemExtensao);
+        ppm.saveHSI(nomeSemExtensao);
     }
 }
--- src/com/company/PPM.java
+++ src/com/company/PPM.java
@@ -114,8 +114,8 @@ public class PPM {
 
     /*
     Salva 3 arquivos PGM, um para cada canal de cor (R, G, B)
-    É necessário passar a extensão
-    Ex: imagem.ppm -> red_imagem.pgm, green_imagem.pgm e blue_imagem.pgm
+    Não é necessário passar a extensão
+    Ex: imagem -> imagem_r.pgm, imagem_g.pgm e imagem_b.pgm
      */
     public void saveRGB(String nomeArq) throws IOException {
         int[][] red_channel = new int[height][width];
@@ -134,9 +134,82 @@ public class PPM {
         PGM green_pgm = new PGM("P2", width, height, maxValue, green_channel);
         PGM blue_pgm = new PGM("P2", width, height, maxValue, blue_channel);
 
-        red_pgm.save("red_"+nomeArq);
-        green_pgm.save("green_"+nomeArq);
-        blue_pgm.save("blue_"+nomeArq);
+        red_pgm.save(nomeArq+"_r.pgm");
+        green_pgm.save(nomeArq+"_g.pgm");
+        blue_pgm.save(nomeArq+"_b.pgm");
+    }
+
+    /*
+    Salva 3 arquivos PGM, um para cada canal (C, M, Y)
+    Não é necessário passar a extensão
+    Ex: imagem -> imagem_c.pgm, imagem_m.pgm e imagem_y.pgm
+     */
+    public void saveCMY(String nomeArq) throws IOException {
+        int[][] c = new int[height][width];
+        int[][] m = new int[height][width];
+        int[][] y = new int[height][width];
+
+        for(int i = 0; i < pixels.length; i++){
+            for (int j = 0; j < pixels[i].length; j++){
+                c[i][j] = maxValue - pixels[i][j][0];
+                m[i][j] = maxValue - pixels[i][j][1];
+                y[i][j] = maxValue - pixels[i][j][2];
+            }
+        }
+
+        PGM c_pgm = new PGM("P2", width, height, maxValue, c);
+        PGM m_pgm = new PGM("P2", width, height, maxValue, m);
+        PGM y_pgm = new PGM("P2", width, height, maxValue, y);
+
+        c_pgm.save(nomeArq+"_c.pgm");
+        m_pgm.save(nomeArq+"_m.pgm");
+        y_pgm.save(nomeArq+"_y.pgm");
+
+    }
+
+    /*
+    Salva 3 arquivos PGM, um para cada canal (H, S, I)
+    Não é necessário passar a extensão
+    Ex: imagem -> imagem_h.pgm, imagem_s.pgm e imagem_i.pgm
+     */
+    public void saveHSI(String nomeArq) throws IOException {
+        int[][] h_channel = new int[height][width];
+        int[][] s_channel = new int[height][width];
+        int[][] i_channel = new int[height][width];
+
+        for(int k = 0; k < pixels.length; k++){
+            for (int j = 0; j < pixels[k].length; j++){
+
+                double r = (double) pixels[k][j][0] / (double) (pixels[k][j][0] + pixels[k][j][1] + pixels[k][j][2]);// R/(R+G+B)
+                double g = (double) pixels[k][j][1] / (double) (pixels[k][j][0] + pixels[k][j][1] + pixels[k][j][2]);// G/(R+G+B)
+                double b = (double) pixels[k][j][2] / (double) (pixels[k][j][0] + pixels[k][j][1] + pixels[k][j][2]);// B/(R+G+B)
+
+                double numerador = 0.5 * ((r-g)+(r-b));
+                double denominador = Math.pow(Math.pow(r-g, 2) + (r-b) * (g-b), 0.5);
+
+                if(denominador == 0) h_channel[k][j] = 0;
+                else {
+                    if(b <= g){
+                        h_channel[k][j] = (int) (Math.acos(numerador / denominador) * (maxValue / (2 * Math.PI)));
+                    }
+                    else{
+                        h_channel[k][j] = (int) (((2 * Math.PI) - (Math.acos(numerador / denominador))) * (maxValue / (2 * Math.PI)));
+                    }
+                }
+
+                s_channel[k][j] = (int) ((1 - 3 * Math.min(Math.min(r, g), b)) * maxValue);
+                i_channel[k][j] = (int) (((pixels[k][j][0] + pixels[k][j][1] + pixels[k][j][2]) / (3.0 * maxValue)) * maxValue);
+            }
+        }
+
+        PGM h_pgm = new PGM("P2", width, height, maxValue, h_channel);
+        PGM s_pgm = new PGM("P2", width, height, maxValue, s_channel);
+        PGM i_pgm = new PGM("P2", width, height, maxValue, i_channel);
+
+        h_pgm.save(nomeArq+"_h.pgm");
+        s_pgm.save(nomeArq+"_s.pgm");
+        i_pgm.save(nomeArq+"_i.pgm");
+
     }
 
     public void showInfo(){
@@ -200,4 +273,5 @@ public class PPM {
     public void setPixels(int[][][] pixels) {
         this.pixels = pixels;
     }
+
 }
